cd5e0078c62a2b71f3f79e996eb57034469b58cb
Micheal Wass <michealw@users.noreply.github.com>
2022-08-30 16:44:09 -0400

    Added get function and parameter type

--- AIApi.cs
+++ AIApi.cs
@@ -1,7 +1,40 @@
-﻿namespace AIApi
+﻿using Newtonsoft.Json;
+
+namespace AIApi
 {
     public class AIApi
     {
-        
+        /// <summary>
+        /// Gets the airport data for the provided code.
+        /// </summary>
+        /// <param name="key">ApiKey object containing the user's API key and host name.</param>
+        /// <param name="type">Airport code type.</param>
+        /// <param name="airport">Airport code to search for.</param>
+        /// <returns>
+        /// Airport object containing the requested data or null
+        /// if the request returned an error.
+        /// </returns>
+        public static async Task<Airport?> GetAirportAsync(ApiKey key, ParameterType type, string airport)
+        {
+            var client = new HttpClient();
+            using var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://airport-info.p.rapidapi.com/airport?{type.ToString().ToLower()}={airport}"),
+                Headers =
+                {
+                    { "X-RapidAPI-Key", key.Key },
+                    { "X-RapidAPI-Host", key.Host }
+                }
+            };
+
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                var airportData = JsonConvert.DeserializeObject<Airport>(jsonResponse);
+                return airportData;
+            }
+        }
     }
 }
---
+++ ParameterType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIApi
+{
+    public enum ParameterType
+    {
+        Iata,
+        Icao
+    }
+}
